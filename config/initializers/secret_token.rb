ef23d53b8a538674fe02f84db4c30d6635309c21
Josh Dean <github@designerdean.com>
2013-05-13 14:47:34 -0600

    default Jekyll stuff

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Megaplex::Application.config.secret_token = '5856a4a6fc799ee84742fea4b5f52946d12bca59fdf059cf990648c1d183945ddd572df278bdd2e72d360e0d29085752acace0476fae96f36ecc4b24c96ce52a'
